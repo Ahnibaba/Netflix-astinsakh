b91853127371a7072ffb7991f030440686e39bb0
Aniebiet Edet <Ahnibaba>
2025-04-27 20:05:34 +0100

    complete APIs

---
+++ server/controllers/search.controller.js
@@ -0,0 +1,124 @@
+import userModel from "../models/user.model.js"
+import { fetchFromTMDB } from "../services/tmdb.service.js"
+
+
+
+const searchPerson = async (req, res) => {
+    const { query } = req.params
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if (response.results.length === 0) {
+            return res.status(404).send(null)
+        }
+
+        await userModel.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory: {
+                  id: response.results[0].id,
+                  image: response.results[0].profile_path,
+                  title: response.results[0].name,
+                  searchType: "person",
+                  createdAt: new Date()
+                }
+            }
+        })
+
+        res.status(200).json({ success: true, content: response.results })
+    } catch (error) {
+        console.log("Error in the searchPerson function: ", error.message)
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+}
+
+const searchMovie = async (req, res) => {
+  const { query } = req.params  
+  try {
+    const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`)
+
+    if (response.results.length === 0) {
+        return res.status(404).send(null)
+    }
+
+    await userModel.findByIdAndUpdate(req.user._id, {
+        $push: {
+            searchHistory: {
+              id: response.results[0].id,
+              image: response.results[0].poster_path,
+              title: response.results[0].title,
+              searchType: "movie",
+              createdAt: new Date()
+            }
+        }
+    })
+
+    res.status(200).json({ success: true, content: response.results })
+
+  } catch (error) {
+    console.log("Error in the searchMovie function: ", error.message)
+    res.status(500).json({ success: false, message: "Internal Server Error" })
+  }
+}
+const searchTv = async (req, res) => {
+  const { query } = req.params  
+  try {
+    const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`)
+
+    if (response.results.length === 0) {
+        return res.status(404).send(null)
+    }
+
+    await userModel.findByIdAndUpdate(req.user._id, {
+        $push: {
+            searchHistory: {
+              id: response.results[0].id,
+              image: response.results[0].poster_path,
+              title: response.results[0].name,
+              searchType: "tv",
+              createdAt: new Date()
+            }
+        }
+    })
+
+    res.status(200).json({ success: true, content: response.results })
+
+  } catch (error) {
+    console.log("Error in the searchTv function: ", error.message)
+    res.status(500).json({ success: false, message: "Internal Server Error" })
+  }
+}
+
+const getSearchHistory = async (req, res) => {
+    try {
+
+        res.status(200).json({ success: true, content: req.user.searchHistory })
+
+    } catch (error) {
+        console.log("Error in the getSearchHistory function: ", error.message)
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+}
+
+
+const removeItemFromSearchHistory = async (req, res) => {
+    const { id } = req.params
+    try {
+
+        await userModel.findByIdAndUpdate(req.user._id, {
+            $pull: {
+                searchHistory: { id: Number(id) }
+            }
+        })
+        
+        res.status(200).json({ success: true, message: `Search item with the id ${id} has been deleted successfully` })
+
+    } catch (error) {
+        console.log("Error in the removeItemFromSearchHistory function: ", error.message)
+        res.status(500).json({ success: false, message: "Internal Server Error" })
+    }
+}
+
+
+
+
+export { searchPerson, searchMovie, searchTv, getSearchHistory, removeItemFromSearchHistory }
